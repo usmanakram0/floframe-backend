7daf76dc873c57e08738765dd8546dd692e657ba
Usman Akram <usmanakram4118@gmail.com>
2025-12-01 13:44:46 +0500

    temp file management for vercel

--- src/routes/extract.route.js
+++ src/routes/extract.route.js
@@ -1,13 +1,16 @@
 import express from "express";
-import upload from "../middleware/fileUpload.js";
-import { extractFrameController } from "../controllers/extract.controller.js";
+import upload from "../middleware/upload.js";
+import { extractLastFrame } from "../controllers/extract.controller.js";
+import cors from "cors";
 
 const router = express.Router();
 
+// Enable CORS specifically for this route
 router.post(
   "/extract-last-frame",
+  cors({ origin: "http://localhost:3000" }),
   upload.single("video"),
-  extractFrameController
+  extractLastFrame
 );
 
 export default router;

2918b4b1e04aa4f2187916f4ce6d41f66922a64a
Usman Akram <usmanakram4118@gmail.com>
2025-12-01 14:36:15 +0500

    push new repod

--- src/routes/extract.route.js
+++ src/routes/extract.route.js
@@ -1,15 +1,13 @@
 import express from "express";
 import upload from "../middleware/upload.js";
-import { extractFrameController } from "../controllers/extract.controller.js"; // ✅ use the controller
-import cors from "cors";
+import { extractFrameController } from "../controllers/extract.controller.js";
 
 const router = express.Router();
 
 router.post(
   "/extract-last-frame",
-  cors({ origin: "http://localhost:3000" }),
   upload.single("video"),
-  extractFrameController // ✅ pass Express handler
+  extractFrameController
 );
 
 export default router;
